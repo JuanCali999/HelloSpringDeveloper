df6048608246361ee2b53322d71e61508e54097d
Juan Calisaya <juancalijesus@gmail.com>
2023-09-15 16:30:42 -0500

    chore: initial commit.

---
+++ src/test/java/com/example/hellospringdeveloper/HelloSpringDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
