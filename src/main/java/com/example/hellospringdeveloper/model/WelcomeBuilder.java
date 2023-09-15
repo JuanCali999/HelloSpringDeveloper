e162305781e2ffd1416e1659f9b02d6ad0a77039
Juan Calisaya <juancalijesus@gmail.com>
2023-09-15 17:05:40 -0500

    fix: added welcome builder.

--- src/main/java/com/example/hellospringdeveloper/model/WelcomeBuilder.java
+++ src/main/java/com/example/hellospringdeveloper/model/WelcomeBuilder.java
@@ -2,7 +2,7 @@ package com.example.hellospringdeveloper.model;
 
 public class WelcomeBuilder {
     private static final String WELCOME_TEMPLATE =
-            "Hello %$!. Now You are a Spring Developer";
+            "Hello %s!. Now You are a Spring Developer";
     public static String build(String name) {
         String finalName = name == null ? "Developer": name;
         return String.format(WELCOME_TEMPLATE, finalName);
